e7c7a6e55cfdb209770b829d028540042e97eef8
Ire Olaniyan <ireolaniyan7@gmail.com>
2020-07-25 16:43:26 +0100

    Feat
    - implement car controller TODOs

---
+++ projects/P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO price (id, currency, price, vehicle_id) VALUES
+(1, 'USD', 12345, 1),
+(2, 'USD', 123456, 2),
+(3, 'USD', 223344, 3),
+(4, 'USD', 334234, 4),
+(5, 'USD', 3123, 5),
+(6, 'USD', 12434, 6),
+(7, 'USD', 34522, 7),
+(8, 'USD', 72464, 8),
+(9, 'USD', 32467, 9);
